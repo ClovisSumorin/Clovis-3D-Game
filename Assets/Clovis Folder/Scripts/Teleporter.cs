d1d9582db1d3227f6554030678b5c15632f11400
srsbsns5 <elyzaescano@gmail.com>
2022-08-30 00:10:31 +0800

    Teleport redone

--- Assets/Clovis Folder/Scripts/Teleporter.cs
+++ Assets/Clovis Folder/Scripts/Teleporter.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
-{
-    public Transform tp;
-
-    public GameObject player;
-
-    private void OnTriggerEnter(Collider other)
+{    
+    public Transform destination;
+    [SerializeField] GameObject teleportObject;
+    private void OnTriggerEnter(Collider other) 
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
-            print("triggered");
-            player.transform.position = tp.transform.position;
+            teleportObject = other.gameObject;
+            StartCoroutine("Teleport");
         }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if (other.tag == "Player")
+        {
+            teleportObject = null;
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(2);
+        teleportObject.SetActive(false);
+        yield return null;
+        teleportObject.transform.position = destination.position;
+        yield return null;
+        teleportObject.SetActive(true);
+        yield return null;
+        teleportObject = null;
     }
 }
